bbe4ea9582a8edfc9885cc9eeecafa09b2c592ae
Nguyen Binh Ho <n_ho18@u.pacific.edu>
2024-10-02 01:24:59 -0700

    three things a timer needs:
    1.Create a timer object (Timer t1)
    2.Setup event handlers (actionPerformed)
    3.Call the timer's start method (t1.start())

--- src/MyFirstTimer.java
+++ src/MyFirstTimer.java
@@ -1,11 +1,17 @@
-import acm.graphics.*;
-import acm.program.*;
+import java.awt.event.ActionEvent;
+
+import javax.swing.Timer;
+
+import acm.graphics.GLabel;
+import acm.program.GraphicsProgram;
 
 public class MyFirstTimer extends GraphicsProgram {
 	public static final int PROGRAM_HEIGHT = 600;
 	public static final int PROGRAM_WIDTH = 800;
 	public static final int MAX_STEPS = 20;
 	private GLabel myLabel;
+	private int numTimes;
+	private Timer t1;
 
 	public void init() {
 		setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
@@ -13,11 +19,22 @@ public class MyFirstTimer extends GraphicsProgram {
 	}
 	
 	public void run() {
+		t1 = new Timer(1000, this);
+		numTimes = 0;
 		myLabel = new GLabel("# of times called?", 0, 100);
 		add(myLabel);
+		t1.setInitialDelay(3000);
+		t1.start();
 	}
 	
 	public static void main(String[] args) {
 		new MyFirstTimer().start();
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		numTimes++;
+		myLabel.setLabel("times called? " + numTimes);
+	    myLabel.move(5, 0);
+	    if(numTimes == 10) t1.stop();
 	}
 }

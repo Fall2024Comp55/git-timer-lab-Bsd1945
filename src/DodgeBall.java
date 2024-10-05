2f168b37dc9786ac935fccc107e8e20c2ad71301
Nguyen Binh Ho <n_ho18@u.pacific.edu>
2024-10-05 01:13:30 -0700

    enemies populate screen regularly

--- src/DodgeBall.java
+++ src/DodgeBall.java
@@ -19,6 +19,7 @@ public class DodgeBall extends GraphicsProgram implements ActionListener {
 	private GLabel text;
 	private Timer movement;
 	private RandomGenerator rgen;
+	private int numTimes;
 	
 	public static final int SIZE = 25;
 	public static final int SPEED = 2;
@@ -31,6 +32,7 @@ public class DodgeBall extends GraphicsProgram implements ActionListener {
 		rgen = RandomGenerator.getInstance();
 		balls = new ArrayList<GOval>();
 		enemies = new ArrayList<GRect>();
+		numTimes = -1;
 		
 		text = new GLabel(""+enemies.size(), 0, WINDOW_HEIGHT);
 		add(text);
@@ -41,7 +43,9 @@ public class DodgeBall extends GraphicsProgram implements ActionListener {
 	}
 	
 	public void actionPerformed(ActionEvent e) {
+		numTimes++;
 		moveAllBallsOnce();
+		if(numTimes % 40  == 0) addAnEnemy();
 	}
 	
 	public void mousePressed(MouseEvent e) {
